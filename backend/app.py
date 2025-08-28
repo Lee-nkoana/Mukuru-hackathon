e6074f3ce57390d3e0c3d8b835cf9d85b484b6be
Lebohang Nkoana <lenkoanjhb024@student.wethinkcode.co.za>
2025-08-28 13:25:04 +0200

    added temp app.py file

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(
+    __name__,
+    template_folder="../frontend/templates",
+    static_folder="../frontend/static"
+)
+
+# Route for the homepage (login page)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def login():
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
