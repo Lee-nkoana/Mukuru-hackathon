3e1b9883ea3954a3c54acc25ffb87d1addcc0ded
Lonwabo Zikhali <zikhalilonwabo@gmail.com>
2025-08-28 15:40:26 +0200

    (Database) Added Clear all before recreating tables in RewardsData

--- backend/database/RewardsDatabase.py
+++ backend/database/RewardsDatabase.py
@@ -14,9 +14,27 @@ class Points(Base):
     points = Column(Integer)
 
 class Tiers(Base):
-    __tablename__ = "Member Level"
+    __tablename__ = "Tiers"
     id = Column(Integer, primary_key=True)
-    tier = Column()
+    tier = Column(String)
 
-
+Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
+
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+# create
+
+Silver = Tiers(tier="Silver")
+Gold = Tiers(tier="Gold")
+Platinum = Tiers(tier="Platinum")
+
+session.add_all([Silver, Gold, Platinum])
+
+session.commit()
+
+# read
+
