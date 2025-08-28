c69516b1c7e999fa54356587bcc23c096af0659e
tsmokonjhb024 <tsmokonjhb024@student.wethinkcode.co.za>
2025-08-28 13:56:07 +0200

    Updated UserDataBase File

---
+++ backend/database/UserDataBase.py
@@ -0,0 +1,22 @@
+#Users
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+#DataBase SetUp
+DataBase_URL = "sqlite:///coding.db"
+
+engine = create_engine(DataBase_URL, echo =True)
+
+# declarative base:
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer,primary_key=True)
+    name = Column(String)
+    email = Column(String,unique=True)
+    
+#Create tables
+Base.metadata.create_all(engine) 
