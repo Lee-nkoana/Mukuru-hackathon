822db16634dd4889ec990c7cfa2d12a04949b18f
Lonwabo Zikhali <zikhalilonwabo@gmail.com>
2025-08-28 13:58:30 +0200

    (Database) Restructured. Added Rewards Database

---
+++ backend/database/RewardsDatabase.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+import sqlite3
+
+engine = create_engine('sqlite:///database.db', echo=True)
+
+class Base(DeclarativeBase):
+    pass
+    
+class Points(Base):
+    __tablename__ = "Points"
+    id = Column(Integer, primary_key=True)
+    Activity = Column(String)
+    Points = Column(Integer)
+
+# class Tiers(Base):
+#     __tablename__ = "Member Level"
+#     id = Column(Integer, primary_key=True)
+
+Base.metadata.create_all(engine)
